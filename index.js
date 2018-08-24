0491bb80c74dca1d33518421bd4be0011cbd0820
Cristiano Fromagio <fromagio_cristiano@live.com>
2018-08-24 11:36:48 -0300

    ⚡️ Sentinel v1.0

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-let hello = 'world';
+const checkLinks = require('check-links')
+const notifier = require('node-notifier')
+const path = require('path')
 
-console.log(`Start: ${hello}`);
+const urlList = require('./url-list')
+
+async function runTests() {
+
+    let results
+
+    try {
+        results = await checkLinks(urlList)
+    } catch (e) {
+        console.log(e)
+    }
+
+    return displayData(results)
+
+}
+
+// setInterval(() => {
+//     runTests()
+// }, 1000 * 60)
+
+runTests()
+
+function displayData(resultsFetch) {
+
+    let message = '';
+
+    for (let i = 0; i < urlList.length; i++) {
+
+        if (resultsFetch[urlList[i]].statusCode != 200) {
+            console.log(`${urlList[i]} `, resultsFetch[urlList[i]])
+            message += '\n' + urlList[i]
+        }
+
+    }
+    
+    if (message.length > 0) {
+        notifier.notify({
+            title: 'Sites fora do ar!',
+            message: 'Os sites estão fora do ar:' + message,
+            icon: path.join(__dirname, 'error-icon.png'),
+            sound: true
+        })
+    }
+
+}
